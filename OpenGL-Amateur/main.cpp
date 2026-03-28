9b3663fd5b2b71a2b1deca643220bbed4795a37e
Kevin Bhavsar <kbhavsar9@gatech.edu>
2026-03-28 10:00:49 -0400

    Initial Commit

---
+++ OpenGL-Amateur/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL-Amateur
+//
+//  Created by Kvvvn on 3/28/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
